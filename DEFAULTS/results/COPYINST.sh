03fb03a8d3139bcb86a3bfc49dac6f547a994a0d
HuaiyuWEI <99250400+HuaiyuWEI@users.noreply.github.com>
2022-06-10 17:16:11 +0800

    Add files via upload

---
+++ DEFAULTS/results/COPYINST.sh
@@ -0,0 +1,14 @@
+mkdir INST
+mv UVEL_inst.* ./INST
+mv VVEL_inst.* ./INST
+mv WVEL_inst.* ./INST
+mv THETA_inst.* ./INST
+mv SALT_inst.* ./INST
+mv PHIHYD_inst.* ./INST
+mv TRAC01_inst.* ./INST
+mv TRAC02_inst.* ./INST
+mv TRAC03_inst.* ./INST
+mv TRAC04_inst.* ./INST
+mv *_inst.* ./INST
+mv STD* ./INST
+
